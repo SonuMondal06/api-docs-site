01cb4a7e7a211e4574f3a7ccd734d5cb2fe0490d
SonuMondal06 <sonu@memorang.com>
2024-11-14 14:59:00 +0530

    Add t3 env

---
+++ src/constants/github.ts
@@ -0,0 +1,9 @@
+import { env } from "@/env";
+
+export const githubDetails = {
+	owner: "SonuMondal06",
+	repo: "content-stack",
+	directory: "content",
+	treeSha: "main",
+	accessToken: env.GITHUB_ACCESS_TOKEN,
+};
